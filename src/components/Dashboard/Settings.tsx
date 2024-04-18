bd38a4c7a43e54e7c87857d6b2f14692d9871a15
ibrahim <74019633+ibrahemaskar11@users.noreply.github.com>
2024-04-18 13:08:21 +0200

    started with settings

---
+++ src/components/Dashboard/Settings.tsx
@@ -0,0 +1,12 @@
+const Settings = () => {
+  return (
+    <div className=" h-full card-shadow  pt-4 bg-white ml-[17.5rem] rounded-3xl  mr-[2rem] ">
+      <h1 className="abel text-[3rem] leading-[3.5rem] px-8 ">Settings</h1>
+      <h3 className="abel capitalize text-[1rem] px-8">
+        Customize Your Experience: Access and Modify Your Personal Settings
+      </h3>
+    </div>
+  );
+};
+
+export default Settings;
